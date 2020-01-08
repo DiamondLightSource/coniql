0fa8735d97b28dfa8bcea6ecc2a98e7060fad36f
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-01-08 13:36:10 +0000

    Laid groundwork for device plugin

--- coniql/deviceplugin.py
+++ coniql/deviceplugin.py
@@ -1,5 +1,11 @@
-from typing import List
+from typing import List, Dict, Any, TypeVar
 from dataclasses import dataclass
+
+from device.types.channel import ReadOnlyChannel
+
+DEVICES: Dict[str, Any] = {}
+CHANNELS: Dict[str, Any] = {}
+
 
 DeviceAddress = List[str]
 
@@ -9,3 +15,14 @@ class ChannelAddress:
     device_address: DeviceAddress
     channel_name: str
 
+
+T = TypeVar('T')
+C = TypeVar('C', bound=ReadOnlyChannel)
+
+
+def register_device(addr: DeviceAddress, device: T):
+    DEVICES[addr] = device
+
+
+def register_channel(addr: ChannelAddress, channel: C):
+    CHANNELS[addr] = channel
