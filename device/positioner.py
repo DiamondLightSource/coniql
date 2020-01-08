9fd9b9eccb1c8bb51851756572f1cbb179c82067
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-01-08 08:44:32 +0000

    Made channels for positioner

--- device/positioner.py
+++ device/positioner.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
 
+from coniql.util import doc_field
+from device.types.channel import ReadOnlyChannel, ReadWriteChannel
+
 
 @dataclass
 class Positioner:
     """Abstract representation of a device that minimises error between a
     setpoint and an indicated position e.g. a motor or temperature controller"""
-    pass
+    position: ReadOnlyChannel = doc_field(
+        "The current indicated position")
+    setpoint: ReadWriteChannel = doc_field(
+        "The target position")
