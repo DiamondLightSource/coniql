3d9fb3ff034e09fbe74ee9b0cc730ff8db8481d7
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-01-14 10:18:09 +0000

    Added velocity channel to motor

--- device/motor.py
+++ device/motor.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass
 from device.joggable import Joggable
 from device.pidcontroller import PidController
 from device.positioner import Positioner
+from device.types.channel import ReadWriteChannel
 
 
 @dataclass
 class Motor(Positioner, Joggable, PidController):
-    pass
+    velocity: ReadWriteChannel[float]
