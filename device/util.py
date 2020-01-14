64adf25cca2b9a8ad5df7e90f0c5ca71bc327a11
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-01-14 15:52:26 +0000

    First attempt and multi-put and multi-get functions for channels

---
+++ device/util.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from typing import Dict, Any, List
+
+from device.devicetypes.channel import ReadWriteChannel, ReadOnlyChannel
+from device.devicetypes.result import Result
+
+_PUT_DICT = Dict[ReadWriteChannel, Any]
+_READBACK_DICT = Dict[ReadOnlyChannel, Result[Any]]
+
+
+async def put_all(channel_values: _PUT_DICT) -> _READBACK_DICT:
+    results = await asyncio.gather(*(channel.put_async(value)
+                                     for channel, value in channel_values.items()))
+    return {channel: result
+            for channel, result in zip(channel_values.keys(), results)}
+
+
+async def get_all(channels: List[ReadOnlyChannel]) -> _READBACK_DICT:
+    results = await asyncio.gather(*(channel.get_async() for channel in channels))
+    return {channel: result
+            for channel, result in zip(channels, results)}
