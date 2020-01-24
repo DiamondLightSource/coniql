28b450ecd104fbe3ce05d2f3ffde0d32c42d0759
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-01-24 10:22:42 +0000

    Removed duplication on cachannel implementation

--- device/cothread/channel.py
+++ device/cothread/channel.py
@@ -8,9 +8,8 @@ from cothread.aioca import caput_one, caget_one, camonitor, FORMAT_TIME
 from coniql._types import ChannelQuality, NumberType, Range, NumberDisplay, \
     DisplayForm
 from device.devicetypes.channel import ReadOnlyChannel, ReadWriteChannel, \
-    DEFAULT_TIMEOUT
+    DEFAULT_TIMEOUT, WriteableChannel, ReadableChannel, MonitorableChannel
 from device.devicetypes.result import Readback
-
 
 NUMBER_TYPES = {
     dbr.DBR_CHAR: NumberType.INT8,
@@ -19,7 +18,6 @@ NUMBER_TYPES = {
     dbr.DBR_FLOAT: NumberType.FLOAT32,
     dbr.DBR_DOUBLE: NumberType.FLOAT64
 }
-
 
 OTHER_TYPES = {
     dbr.DBR_ENUM: "Enum",
@@ -36,51 +34,49 @@ CHANNEL_QUALITY_MAP = [
 ]
 
 EMPTY_RANGE = Range(0, 0)
-EMPTY_DISPLAY = NumberDisplay(EMPTY_RANGE, EMPTY_RANGE, EMPTY_RANGE, EMPTY_RANGE, "", 4, DisplayForm.DEFAULT)
-
+EMPTY_DISPLAY = NumberDisplay(EMPTY_RANGE, EMPTY_RANGE, EMPTY_RANGE,
+                              EMPTY_RANGE, "", 4, DisplayForm.DEFAULT)
 
 T = TypeVar('T')
 
 
-class ReadOnlyCaChannel(ReadOnlyChannel):
-    def __init__(self, pv: str):
+class PvRef:
+    def __init__(self, pv: str, rbv: Optional[str] = None,
+                 rbv_suffix: Optional[str] = None):
         self.pv = pv
+        self.rbv = rbv or f'{pv}{rbv_suffix}' if rbv is not None else None or pv
 
+
+class CaWriter(PvRef, WriteableChannel):
+    async def put(self, value: T, timeout: float = DEFAULT_TIMEOUT) -> \
+            Readback[T]:
+        await caput_one(self.pv, value, timeout=timeout)
+        readback = await caget_one(self.rbv, timeout=timeout)
+        return readback
+
+
+class CaReader(PvRef, ReadableChannel):
     async def get(self, timeout: float = DEFAULT_TIMEOUT) -> Readback[T]:
-        value = await caget_one(self.pv, format=FORMAT_TIME, timeout=timeout)
-        return Readback.ok(value, mutable=False)
+        value = await caget_one(self.rbv, format=FORMAT_TIME, timeout=timeout)
+        return Readback.ok(value, mutable=True)
 
+
+class CaMonitorer(PvRef, MonitorableChannel):
     async def monitor(self) -> AsyncGenerator[Readback[T], None]:
         q = asyncio.Queue()
 
         def queue_callback(value):
             asyncio.create_task(q.put(value))
 
-        m = await camonitor(self.pv, queue_callback, format=FORMAT_TIME)
+        m = await camonitor(self.rbv, queue_callback, format=FORMAT_TIME)
         while True:
             yield Readback.ok(await q.get(), mutable=False)
 
 
-class ReadWriteCaChannel(ReadWriteChannel):
-    def __init__(self, pv: str):
-        self.pv = pv
+class ReadWriteCaChannel(ReadWriteChannel, CaWriter, CaReader,
+                         CaMonitorer):
+    pass
 
-    async def get(self, timeout: float = DEFAULT_TIMEOUT) -> Readback[T]:
-        value = await caget_one(self.pv, format=FORMAT_TIME, timeout=timeout)
-        return Readback.ok(value, mutable=True)
 
-    async def monitor(self) -> AsyncGenerator[Readback[T], None]:
-        q = asyncio.Queue()
-
-        def queue_callback(value):
-            asyncio.create_task(q.put(value))
-
-        m = await camonitor(self.pv, queue_callback, format=FORMAT_TIME)
-        while True:
-            yield Readback.ok(await q.get(), mutable=True)
-        m.close()
-
-    async def put(self, value: T, timeout: float = DEFAULT_TIMEOUT) -> Readback[T]:
-        await caput_one(self.pv, value, timeout=timeout)
-        readback = await self.get(timeout)
-        return readback
+class ReadOnlyCaChannel(ReadOnlyChannel, CaReader, CaMonitorer):
+    pass
