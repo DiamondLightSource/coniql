1f1d6de652ba8866c4780f50a91afc2ed25efbfb
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-01-27 15:22:52 +0000

    Added helper functions to trigger box

--- device/devices/faketriggerbox.py
+++ device/devices/faketriggerbox.py
@@ -1,19 +1,23 @@
 import asyncio
 from collections import Callable
 from dataclasses import dataclass
+from typing import Optional
 
 from device.devicetypes.channel import ReadWriteChannel, ReadableChannel, \
     WriteableChannel
+from device.inmemory.channel import InMemoryReadWriteChannel
 
 _PREDICATE = Callable[[float], bool]
 _SIDE_EFFECT = Callable[[], None]
 
+_DEFAULT_CLOCK_SECONDS = 0.1
+
 
 @dataclass
 class Trigger:
-    input: ReadWriteChannel[ReadableChannel[float]]
-    predicate: ReadWriteChannel[_PREDICATE]
-    output: ReadWriteChannel[WriteableChannel[bool]]
+    input: ReadWriteChannel[Optional[ReadableChannel[float]]]
+    predicate: ReadWriteChannel[Optional[_PREDICATE]]
+    output: ReadWriteChannel[Optional[WriteableChannel[bool]]]
     delay_seconds: ReadWriteChannel[float]
 
     async def run_in_memory(self):
@@ -25,6 +29,15 @@ class Trigger:
         if predicate(current):
             await asyncio.sleep(delay)
             await output_channel.put(True)
+
+
+def in_memory_trigger() -> Trigger:
+    return Trigger(
+        input=InMemoryReadWriteChannel(None),
+        predicate=InMemoryReadWriteChannel(None),
+        output=InMemoryReadWriteChannel(None),
+        delay_seconds=InMemoryReadWriteChannel(0.0)
+    )
 
 
 @dataclass
@@ -42,3 +55,22 @@ class FakeTriggerBox:
             ])
             delay = await self.min_seconds_between_checks.get()
             await asyncio.sleep(delay.value)
+
+    def run_forever(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.run_in_memory())
+
+
+def in_memory_box() -> FakeTriggerBox:
+    return FakeTriggerBox(
+        trigger_1=in_memory_trigger(),
+        trigger_2=in_memory_trigger(),
+        min_seconds_between_checks=InMemoryReadWriteChannel(
+            _DEFAULT_CLOCK_SECONDS)
+    )
+
+
+def in_memory_box_running() -> FakeTriggerBox:
+    box = in_memory_box()
+    box.run_in_memory()
+    return box
