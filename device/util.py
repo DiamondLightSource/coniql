c859d5b873c4132debf11cf2b093d899bb89219d
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-01-27 08:57:48 +0000

    Added util function to await a value from a channel, needs to be generalised

--- device/util.py
+++ device/util.py
@@ -1,17 +1,20 @@
 import asyncio
 
-from typing import Dict, Any, List
+from typing import Dict, Any, List, TypeVar, Optional
+from datetime import datetime, timedelta
 
-from device.devicetypes.channel import ReadWriteChannel, ReadOnlyChannel
-from device.devicetypes.result import Result
+from device.devicetypes.channel import ReadWriteChannel, ReadOnlyChannel, \
+    MonitorableChannel
+from device.devicetypes.result import Readback
 
 _PUT_DICT = Dict[ReadWriteChannel, Any]
-_READBACK_DICT = Dict[ReadOnlyChannel, Result[Any]]
+_READBACK_DICT = Dict[ReadOnlyChannel, Readback[Any]]
 
 
 async def put_all(channel_values: _PUT_DICT) -> _READBACK_DICT:
     results = await asyncio.gather(*(channel.put(value)
-                                     for channel, value in channel_values.items()))
+                                     for channel, value in
+                                     channel_values.items()))
     return {channel: result
             for channel, result in zip(channel_values.keys(), results)}
 
@@ -20,3 +23,18 @@ async def get_all(channels: List[ReadOnlyChannel]) -> _READBACK_DICT:
     results = await asyncio.gather(*(channel.get() for channel in channels))
     return {channel: result
             for channel, result in zip(channels, results)}
+
+
+T = TypeVar('T')
+
+
+# TODO: Generic predicate handling!
+async def await_value(channel: MonitorableChannel[T], value: T,
+                      timeout: Optional[float] = None) -> Optional[Readback[T]]:
+    latest_time = datetime.now() + timedelta(seconds=timeout or 0)
+    async for readback in channel.monitor():
+        if readback.value == value:
+            return readback
+        if timeout is not None and datetime.now() > latest_time:
+            break
+    return None
