d33dcc6f97e1acca47811f96490245341edc2667
Forrester, Callum (DLSLtd,RAL,LSCI) <callum.forrester@diamond.ac.uk>
2020-02-06 08:47:43 +0000

    Very initial definition of some parts of a PandA device

---
+++ device/devices/panda.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from device.devicetypes.channel import ReadWriteChannel
+
+
+@dataclass
+class Bits:
+    """Soft inputs and constant bits"""
+    a: ReadWriteChannel[bool]
+    b: ReadWriteChannel[bool]
+    c: ReadWriteChannel[bool]
+    d: ReadWriteChannel[bool]
+
+
+@dataclass
+class Panda:
+    bits: Bits
