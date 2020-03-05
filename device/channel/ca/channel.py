6bf54cd964278d6e9be8499d411f6e77a4d35ac3
Callum Forrester <callum.forrester@diamond.ac.uk>
2020-03-05 14:59:05 +0000

    Added comments to cachannel

--- device/channel/ca/channel.py
+++ device/channel/ca/channel.py
@@ -13,6 +13,8 @@ T = TypeVar('T')
 
 def find_rbv(pv: str, rbv: Optional[str] = None,
              rbv_suffix: Optional[str] = None) -> str:
+    """Determines readback PV given the PV and optionally a readback PV and
+    a readback suffix."""
     if rbv is not None:
         return rbv
     elif rbv_suffix is not None:
@@ -22,6 +24,7 @@ def find_rbv(pv: str, rbv: Optional[str] = None,
 
 
 class CaChannel(Generic[T]):
+    """A Channel that can communicate with a PV over Channel Access"""
     timeout: float
     wait: bool
     pv: str
@@ -37,26 +40,44 @@ class CaChannel(Generic[T]):
         self.timeout = timeout or 5
 
     async def setup(self):
+        """Performs any setup for this channel. Verifies that the PV is
+        reachable and gather any metadata"""
         return await connect([self.pv, self.rbv])
 
     async def put(self, value: T) -> bool:
-        await self.caput(value)
+        """Write a value to this channel"""
+        await self.__caput(value)
         return True
 
-    async def caput(self, value):
-        return await caput_one(self.pv, value,
-                               timeout=self.timeout,
-                               wait=self.wait)
-
     async def get(self) -> T:
-        value = await self.caget()
+        """Request and read a value from this channel"""
+        value = await self.__caget()
         return self.format_value(value)
 
     async def get_readback(self) -> Readback:
-        value = await self.caget()
+        """Request and read a Readback structure from this channel"""
+        value = await self.__caget()
         return await self.as_readback(value)
 
-    async def as_readback(self, value):
+    async def monitor(self) -> AsyncGenerator[T, None]:
+        """Returns an asynchronous generator that yields values of this
+        channel. The first value is yeilded when the request is made.
+        Subsequent values are yeilded when the channel changes."""
+        gen = await self.__camonitor()
+        async for value in gen:
+            yield value.real
+
+    async def monitor_readback(self) -> AsyncGenerator[Readback, None]:
+        """Returns an asynchronous generator that yields Readback objects
+        associated with this channel. The first value is yeilded when the
+        request is made. Subsequent values are yeilded when the channel
+        changes."""
+        gen = await self.__camonitor()
+        async for value in gen:
+            yield self.as_readback(value)
+
+    async def as_readback(self, value) -> Readback:
+        """Converts a value structure from aioca to a Readback object"""
         time, status = value_to_readback_meta(value)
         return Readback(
             value=self.format_value(value),
@@ -65,22 +86,21 @@ class CaChannel(Generic[T]):
         )
 
     def format_value(self, value) -> T:
+        """Converts an aioca value into a simpler value. Should be used for
+        formatting and conversion to a simpler Python type"""
         return value.real
 
-    async def caget(self):
+    # Simple wrappers around CA functions
+
+    async def __caput(self, value):
+        return await caput_one(self.pv, value,
+                               timeout=self.timeout,
+                               wait=self.wait)
+
+    async def __caget(self):
         return await caget_one(self.rbv,
                                format=FORMAT_TIME,
                                timeout=self.timeout)
 
-    async def monitor_readback(self) -> AsyncGenerator[Readback, None]:
-        gen = await self.camonitor()
-        async for value in gen:
-            yield self.as_readback(value)
-
-    async def monitor(self) -> AsyncGenerator[T, None]:
-        gen = await self.camonitor()
-        async for value in gen:
-            yield value.real
-
-    async def camonitor(self):
+    async def __camonitor(self):
         return camonitor_as_async_generator(self.rbv)
