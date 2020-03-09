9fe89561aa2a3e0f0b82e24ad94eac2fd9ac7508
Callum Forrester <callum.forrester@diamond.ac.uk>
2020-03-09 08:52:48 +0000

    Documented YAML type

--- device/core/yaml/yamltype.py
+++ device/core/yaml/yamltype.py
@@ -9,17 +9,30 @@ from device.viewableasdict import ViewableAsDict
 
 
 def yaml_load(yaml_path: str, **kwargs):
+    """Shortcut to creating and instantiating a YAML type. Good for the root of
+    a device tree if the type only needs to be used once."""
     return yaml_type(yaml_path)(**kwargs)
 
 
 def yaml_type(yaml_path: str) -> Type:
+    """Dynamically constructs a new type from a YAML definition, creates a
+    constructor which takes any variables in the file using the $(<var name>)
+    syntax as keyword arguments."""
+
     text = load_and_preprocess(yaml_path)
 
     def make_raw_structure(**kwargs):
+        """Replaces the variables according to kwargs and returns a new
+        dictionary representing the YAML."""
         to_parse = replace_substitutions(text, kwargs)
         return yaml.load(to_parse)
 
     def make_args(**kwargs):
+        """Turns the top-level elements of the dictionary into objects via the
+        following rule:
+        ['type'] is assumed to be the address of the type within the Python
+        path. All other key value pairs are given as keyword arguments to the
+        constructor."""
         raw_structure = make_raw_structure(**kwargs)
         fields = {
             name: field_from_dict_def(yaml_def)
@@ -28,6 +41,8 @@ def yaml_type(yaml_path: str) -> Type:
         return fields
 
     class YamlType:
+        """Dynamic type just for this file. TODO: Rename it to the file name."""
+
         def __init__(self, **kwargs):
             args = make_args(**kwargs)
             self.__dict__.update(**args)
@@ -48,6 +63,8 @@ def yaml_type(yaml_path: str) -> Type:
 
 
 def replace_substitutions(value: str, substitutions: Dict[str, str]) -> str:
+    """Replaces variables using the $(<var name>) syntax according to the
+    mapping provided."""
     for s in substitutions:
         value = value.replace("$(%s)" % s, str(substitutions[s]))
     return value
