58f0c3e837d8c4147ed8aacae7eccbfbbe03b39f
Callum Forrester <callum.forrester@diamond.ac.uk>
2020-03-11 08:32:04 +0000

    Roughly defined parts of zebra in YAML

---
+++ device/zebra/__init__.py
@@ -0,0 +1,6 @@
+from device.core.yaml.yamltype import yaml_type
+
+Pulse = yaml_type('device/zebra/pulse.yaml')
+PositionCapture = yaml_type('device/zebra/positioncapture.yaml')
+ZebraIO = yaml_type('device/zebra/ZebraIO.yaml')
+Zebra = yaml_type('device/zebra/Zebra.yaml')
