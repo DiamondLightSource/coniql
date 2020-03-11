1029052fea56e5aec0febb17375235924722f411
Callum Forrester <callum.forrester@diamond.ac.uk>
2020-03-11 09:18:39 +0000

    Extracted AD plugin common fields to YAML

---
+++ device/adcore/plugin/__init__.py
@@ -0,0 +1,6 @@
+from device.core.yaml.yamltype import yaml_type
+
+Callback = yaml_type('device/adcore/plugin/callback.yaml')
+Counters = yaml_type('device/adcore/plugin/counters.yaml')
+Properties = yaml_type('device/adcore/plugin/properties.yaml')
+AdPlugin = yaml_type('device/adcore/plugin/adplugin.yaml')
