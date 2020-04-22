2f7f532e1594867c78fbc93e526df05ab298acb4
Tim Guite <tim.guite@diamond.ac.uk>
2020-04-22 16:07:28 +0100

    Updated test to look more like the python test script
    Will next refactor this into a function and test it at a variety of frequencies and wave sizes, then make direct comparison with python client

--- benchmark/js_client_test.js
+++ benchmark/js_client_test.js
@@ -1,25 +1,50 @@
 const { query } = require("graphqurl");
 
-function eventCallback(event) {
-  console.log("Event received:", event);
-  // handle event
-}
-
-function errorCallback(error) {
+const errorCallback = (error) => {
   console.log("Error:", error);
-}
+};
 
-query(
-  {
-    query: `subscription {
+let count = 0;
+
+query({
+  query: `subscription {
         subscribeChannel(id: "sim://sinewavesimple(1000,0.1)") {
           id
           value
         }
       }
       `,
-    endpoint: "ws://localhost:8000/subscriptions"
-  },
-  eventCallback,
-  errorCallback
-);
+  endpoint: "ws://localhost:8000/subscriptions",
+  headers: {
+    id: 1
+  }
+})
+  .then((observable) => {
+    const s = observable.subscribe(
+      (event) => {
+        // console.log("Event received", event);
+        count++;
+      },
+      (error) => {
+        console.log("Error", error);
+        // handle error
+      }
+    );
+    return s;
+  })
+  .then((s) => {
+    const t1 = process.hrtime();
+    setTimeout(() => {
+      s.unsubscribe();
+      const t2 = process.hrtime(t1);
+      const executionTime = (t2[0] * 1000 + t2[1] / 1000000) / 1000;
+      console.log("Unsubbing...");
+      console.log(`Final count: ${count}`);
+      console.info("Execution time (hr): %ds %dms", t2[0], t2[1] / 1000000);
+      console.log(`Execution Time: ${executionTime}`);
+      const messageFreq = count / executionTime;
+      console.info(`Measured frequency: ${messageFreq} Hz`);
+      process.exit(0);
+    }, 10000);
+  })
+  .catch(errorCallback);
