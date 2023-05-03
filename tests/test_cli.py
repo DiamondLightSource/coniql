9ed464869f4214356c71f4f3ce3d8ff0e43983b3
Andy Wilson <andrew.wilson@diamond.ac.uk>
2023-05-03 15:03:10 +0100

    Remove boilerplate test file from skeleton

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-from python3_pip_skeleton import __version__
-
-
-def test_cli_version():
-    cmd = [sys.executable, "-m", "python3_pip_skeleton", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
