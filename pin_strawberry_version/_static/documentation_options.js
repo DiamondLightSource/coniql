486d637cff0cc74fb7dea74000edc8ac1ed43d88
rjwills28 <rjwills28@users.noreply.github.com>
2023-06-21 09:54:26 +0000

    deploy: 708cc3d95e8ffe568850dc9b982a1d1bcc18090f

--- pin_strawberry_version/_static/documentation_options.js
+++ pin_strawberry_version/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.dev11+gadd0134',
+    VERSION: '0.4.dev12+g708cc3d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
