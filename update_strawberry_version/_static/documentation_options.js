7b8541bae7df4ce5b70f58db7072afe85384b7dd
rjwills28 <rjwills28@users.noreply.github.com>
2023-06-29 13:23:20 +0000

    deploy: 9cb88a3df13087e0a485679e7b56b10e0fedfe26

--- update_strawberry_version/_static/documentation_options.js
+++ update_strawberry_version/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.dev34+gbe605b8',
+    VERSION: '0.4.dev35+g9cb88a3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
