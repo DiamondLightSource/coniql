2c234e0f4b4555e1dbd05eb3de2149f9362271fc
AlexanderWells-diamond <AlexanderWells-diamond@users.noreply.github.com>
2023-07-10 12:03:39 +0000

    deploy: 86054d9ef88e5838cbd1291fdb10f438782605d2

--- kubernetes_performance_test/_static/documentation_options.js
+++ kubernetes_performance_test/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.dev51+g4dfdf64',
+    VERSION: '0.4.dev51+g86054d9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
