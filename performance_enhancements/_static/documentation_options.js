a9513f1a4d81074bb25ebeab890501f0f58754c2
AlexanderWells-diamond <AlexanderWells-diamond@users.noreply.github.com>
2023-08-09 08:32:01 +0000

    deploy: cf2d53077432168d2ada3a93d4c474fefac833c5

--- performance_enhancements/_static/documentation_options.js
+++ performance_enhancements/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.dev76+gfe36404',
+    VERSION: '0.4.dev70+gcf2d530',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
