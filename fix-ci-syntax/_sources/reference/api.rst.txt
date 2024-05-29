c2d6eec4da1b94e71b1db22a9163841048f2c58b
abigailalexander <abigailalexander@users.noreply.github.com>
2024-05-29 09:33:13 +0000

    deploy: 8b7824f144414fba2f50aeb7544a16e373eff06c

---
+++ fix-ci-syntax/_sources/reference/api.rst.txt
@@ -0,0 +1,15 @@
+API
+===
+
+.. automodule:: coniql
+    :members:
+
+    ``coniql``
+    -----------------------------------
+
+This is the internal API reference for coniql
+
+.. data:: coniql.__version__
+    :type: str
+
+    Version number as calculated by https://github.com/dls-controls/versiongit
---
+++ fix-ci-syntax/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.4.3.dev8+g8b7824f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
